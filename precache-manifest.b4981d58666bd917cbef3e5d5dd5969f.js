f55556b901f8c627777a4b465d99fd8d2597d478
Shubhashis Roy <iamdipta@gmail.com>
2020-03-07 21:18:51 +0600

    Updates

---
+++ precache-manifest.b4981d58666bd917cbef3e5d5dd5969f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bd4a7df0f4e3b2ea865df9c415b5a29",
+    "url": "/slack/index.html"
+  },
+  {
+    "revision": "1e75fd56b0edefa09588",
+    "url": "/slack/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8ca1cbbbc4fbd9f3e35e",
+    "url": "/slack/static/js/2.1704af06.chunk.js"
+  },
+  {
+    "revision": "0aa1458bc7cacdca51b725f63f317868",
+    "url": "/slack/static/js/2.1704af06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e75fd56b0edefa09588",
+    "url": "/slack/static/js/main.c78d26ac.chunk.js"
+  },
+  {
+    "revision": "7638cf476d3f1311fee7",
+    "url": "/slack/static/js/runtime-main.7ea8468b.js"
+  }
+]);
